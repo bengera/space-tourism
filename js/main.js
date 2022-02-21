7c7eb857df3603fdaef3c87889a598ea0bc17915
unknown <bengera89@gmail.com>
2022-02-21 15:05:49 +0700

    mobile-nav

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const hamMenu = document.querySelector('.nav-toggle');
+const navUL = document.querySelector('.navigation-mobile');
+
+hamMenu.addEventListener('click',() => {
+    console.log('toggle-menu');
+    hamMenu.classList.toggle('active');
+    navUL.classList.toggle('show');
+    
+    
+});
+
